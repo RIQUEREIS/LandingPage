3ccf27018e4ccbb49b71a76f0a7b3b2720b06c76
Matheus Reimer <matheusreimer1@gmail.com>
2022-02-28 12:24:20 -0300

    second commit

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -3,12 +3,15 @@ import {useState} from 'react'
 import ReactDOM from 'react-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Placeholder from "../../assets/placeholder.png";
+import Star from "../../assets/star.png";
+import Secure from "../../assets/Secure.png";
+import Email from "../../assets/email.png";
+import Check from "../../assets/check.png";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { WhatsApp } from "@material-ui/icons";
 
 
 export default function Works() {
-
- 
-
     return (
       <div className="greenPart" id="greenPart">
       <div className="greenHolder">
@@ -30,6 +33,7 @@ export default function Works() {
               </div>
               <div className="column2">
                 <img src={Placeholder}></img>
+                <img src="https://cursodespertemilionario.com/wp-content/uploads/2021/06/Design-sem-nome.png"></img>
               </div>
               <div className="column3">
               <div className="card">
@@ -43,10 +47,17 @@ export default function Works() {
                 </div>
               </div>
           </div>
+          
       </div>
+
       <div className="bottom">
-        <p>test</p>
+        <a><p><img src={Star}     ></img> Privacidade</p>Sua informacao 100% segura</a>
+        <a><p><img src={Secure}   ></img> Compra segura</p>Ambiente seguro e autenticado</a>
+        <a><p><img src={Email}    ></img> Entregue via E-mail</p>Produto entregue por e-mail</a>
+        <a><p><img src={Check}    ></img> Conteudo aprovado</p>100% revisado e aprovado</a>
+
       </div>
+
       </div>
     );
   }
